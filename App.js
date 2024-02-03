37a9c7797fcbc1d696324ffbf9d20595cbf81960
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-02-03 19:32:31 +0530

    react-nested-sibling

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
 /*
 
+
+
+
+
+
 <div id="parent">
 <div>
 <h1>
@@ -10,24 +15,33 @@ hi am h1 tag
 </div>
 */
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "I'm an h1 tag")
-  )
-);
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement(
+//     "div",
+//     { id: "child" },
+//     React.createElement("h1", {}, "I'm an h1 tag")
+//   )
+// );
+
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading", xyz: "abc" },
+//   "Hello World from react"
+// );
+// id:attribute ex <h1 id="heading" xyz="abc">Hello World</h1>
 
 const heading = React.createElement(
-  "h1",
-  { id: "heading", xyz: "abc" },
-  "Hello World from react"
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm an h2 tag"),
+  ])
 );
-// id:attribute ex <h1 id="heading" xyz="abc">Hello World</h1>
-console.log(parent);
+console.log(heading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(heading);
