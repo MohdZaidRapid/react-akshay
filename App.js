78f05f969d24cfc5d3a2c89990bc2ff1031f5f70
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-02-03 19:24:10 +0530

    react-nested-element

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
+/*
+
+<div id="parent">
+<div>
+<h1>
+hi am h1 tag
+</h1>
+</div>
+
+</div>
+*/
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "I'm an h1 tag")
+  )
+);
+
 const heading = React.createElement(
   "h1",
   { id: "heading", xyz: "abc" },
   "Hello World from react"
 );
 // id:attribute ex <h1 id="heading" xyz="abc">Hello World</h1>
+console.log(parent);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(parent);
