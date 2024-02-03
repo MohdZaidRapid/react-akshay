aa78802e35a33a70e685bf555fb40b1d61f8e9e8
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-02-03 19:44:15 +0530

    react-nested-sibling

--- App.js
+++ App.js
@@ -32,14 +32,16 @@ hi am h1 tag
 // );
 // id:attribute ex <h1 id="heading" xyz="abc">Hello World</h1>
 
-const heading = React.createElement(
-  "div",
-  { id: "parent" },
+const heading = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "I'm an h1 tag"),
     React.createElement("h2", {}, "I'm an h2 tag"),
-  ])
-);
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm an h2 tag"),
+  ]),
+]);
 console.log(heading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
